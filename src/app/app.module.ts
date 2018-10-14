bf7fc76ed31db28c693e515ac922da25527cbf50
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-10-15 00:33:42 +0200

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoursesComponent } from './courses.component';
+import { CoursesService } from './courses.service';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
-    BrowserModule
+  
+BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
